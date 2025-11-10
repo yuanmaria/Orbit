31a1c82ac345fae8124e7be76166c84c29d8ff2b
yuanmaria <yuanita.maria@vonix.id>
2025-11-10 13:41:18 +0700

    fix: separate route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/OrbitView.vue'),
+    },
+    {
+      path: '/opera',
+      name: 'orbit-opera',
+      component: () => import('../views/OrbitOpera.vue'),
+    },
+  ],
 })
 
 export default router
---
+++ src/views/OrbitView.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="orbit">
+    <!-- Orbit Background Image -->
+    <img class="bg" src="/orbit/orbit-bg.png" alt="" />
+
+    <!-- OUTER RING (clockwise) -->
+    <img class="planet outer" style="--phase: 0%" src="/orbit/item5.svg" alt="" />
+    <img class="planet outer" style="--phase: 25%" src="/orbit/item6.svg" alt="" />
+    <img class="planet outer" style="--phase: 50%" src="/orbit/item7.svg" alt="" />
+    <img class="planet outer" style="--phase: 75%" src="/orbit/item8.svg" alt="" />
+
+    <!-- INNER RING (counter-clockwise) -->
+    <img class="planet inner" style="--phase: 0%" src="/orbit/item1.svg" alt="" />
+    <img class="planet inner" style="--phase: 25%" src="/orbit/item2.svg" alt="" />
+    <img class="planet inner" style="--phase: 50%" src="/orbit/item3.svg" alt="" />
+    <img class="planet inner" style="--phase: 75%" src="/orbit/item4.svg" alt="" />
+  </div>
+</template>
+
+<style scoped></style>
